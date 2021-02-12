9ca0e323f2a77dcd0b973e97717c9ae888ebdd1e
PatrickLSteward <70727884+PatrickLSteward@users.noreply.github.com>
2021-02-12 10:41:49 -0500

    Update hello_functions.h

--- hello_functions.h
+++ hello_functions.h
@@ -1,16 +1,15 @@
 #ifndef MAIN_H
 
-#define MAIN_H
+#define HELLO_FUNCTIONS_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 // define functions here
-void a_practice();
-void v_practice();
-void p_practice();
-void s_practice();
-void j_practice();
+
+int add(int a, int b);
+float temp_sensor_converter(int temp_sensor_value, bool *success);
+float temp_c_to_f(float temp_c);
   
 #endif  
