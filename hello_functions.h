7fe997d60671a61f66ab1c231f98789d34315971
PatrickLSteward <70727884+PatrickLSteward@users.noreply.github.com>
2021-02-12 10:35:28 -0500

    Create hello_functions.h
    This is the header file for various functions

---
+++ hello_functions.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// define functions here
+void a_practice();
+void v_practice();
+void p_practice();
+void s_practice();
+void j_practice();
+  
+#endif  
