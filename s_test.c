a865a690ec6e2ad26168630681cf8aa508606339
PatrickLSteward <70727884+PatrickLSteward@users.noreply.github.com>
2021-02-12 10:30:29 -0500

    Create s_test.c

---
+++ s_test.c
@@ -0,0 +1,36 @@
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include "main.h"
+
+//the add function is a stub
+int add(int a, int b){
+  return a + b;
+}
+
+
+void test_add(void)
+{
+    CU_ASSERT(add(2,2) == 4);
+    CU_ASSERT(add(0, 0) == 0);
+    CU_ASSERT(add(-1, 1) == 0);
+}
+
+int main() {
+
+    CU_initialize_registry();
+
+    CU_pSuite suite1 = CU_add_suite("test_add", 0, 0);
+    
+    CU_add_test(suite1, "test_add", test_add);
+    
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+
+    CU_basic_run_tests();
+
+    CU_cleanup_registry();
+
+
+
+    return 0;
+
+}
